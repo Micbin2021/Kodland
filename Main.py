a4a60f71744e6fc29839d65a25f14b33749a6f55
Micbin2021 <binczyckimi@gmail.com>
2024-10-13 07:42:12 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,43 @@
+import discord
+import bcrypt
 import random
-fakty = ["najbardziej lubię programować","mam 2 kotki","nie mam ulubionego dania","mam najsłodze zwierzęta"]
-print(random.choice(fakty))
+
+intents = discord.Intents.default()
+intents.message_content = True
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowano jako {bot.user.name}')
+
+@bot.command()
+async def hej(ctx):
+    await ctx.send('Hej Hej !')
+
+@bot.command()
+async def powtarzanie(ctx, count=5):
+    await ctx.send('test ' * count)
+
+@bot.command()
+async def suma(ctx, a: int, b: int):
+    await ctx.send(f'Suma liczb {a} + {b} = {a + b}')
+
+@bot.command()
+async def generator(ctx,lenght: int):
+    symbols = "+-/*?=@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+    for i in range(lenght):
+        password += random.choice(symbols)
+    await ctx.send(f'Twoje hasło to: {password}')
+
+@bot.command()
+async def rzut(ctx):
+    mozliwosci = ("orzeł","reszka")
+    wynik = random.choice(mozliwosci)
+    await ctx.send(f'Rzut padł na {wynik}')
+
+
+
+bot.run('')
